aa670aa2798e963e0d4539fdafe2903dce48236c
Brisasanchez16 <brisncz@gmail.com>
2022-06-21 20:12:40 -0300

    carrito

--- js/carrito.js
+++ js/carrito.js
@@ -14,6 +14,66 @@ let sahumerioR = 450
 let si = "si"
 
 
+function carritoCristales(){
+    let cristales = prompt ("Quiere añadir Cristales a su carrito?")
+    if (cristales == si){
+        let cantAmaz = prompt ("Cuantas amazonitas va a llevar?")
+        let cantAmat = prompt ("Cuantas amatistas va a llevar?")
+        let cantAve = prompt ("Cuantas aventurinas va a llevar?")
+        let cantCit = prompt ("Cuantos citrinos va a llevar?")
+        let cantCuar = prompt ("Cuantos cuarzos rosa va a llevar?")
+        let cantFlu = prompt ("Cuantas fluoritas verdes va a llevar?")
+
+        let sumaAmaz = (amazonita * cantAmaz)
+        let sumaAmat = (amatista *cantAmat)
+        let sumaAve = (aventurina * cantAve)
+        let sumaCit = (citrino * cantCit)
+        let sumaCuar = (cuarzoRosa * cantCuar)
+        let sumaFlu = (fluoritaVerde * cantFlu)
+
+        let resultadoCri = (sumaAmaz + sumaAmat + sumaAve + sumaCit + sumaCuar + sumaFlu)
+
+        alert("$" + resultadoCri)
+    }
+}
+
+function carritoHierbas(){
+    let hierbas = prompt ("Quiere añadir Hierbas a su carrito?")
+    if (hierbas == si){
+        let cantKit = prompt ("Cuantos kit de hierbas va a llevar?")
+        let cantCal = prompt ("Cuantas bolsitas de calendulas va a llevar?")
+        let cantMen = prompt ("Cuantas bolsitas de menta a llevar?")
+
+        let sumaKit = (kitHierbas * cantKit)
+        let sumaCal = (calendula *cantCal)
+        let sumaMen = (menta * cantMen)
+
+        let resultadoHier = (sumaKit + sumaCal + sumaMen)
+
+        alert("$" + resultadoHier)
+    }
+}
+
+function carritoIncienso(){
+    let incienso = prompt ("Quiere añadir inciensos a su carrito?")
+    if (incienso == si){
+        let cantSps = prompt ("Cuantos sahumerios de palo santo va a llevar?")
+        let cantSr = prompt ("Cuantos sahumerios de rosas va a llevar?")
+
+        let sumaSps = (sahumerioPS * cantSps)
+        let sumaSr = (sahumerioR *cantSr)
+
+        let resultadoInc = (sumaSps + sumaSr)
+
+        alert("$" + resultadoInc)
+    }
+}
+
+carritoCristales()
+carritoHierbas()
+carritoIncienso()
+
+
 function addInfo(){
     let inputNombre= document.getElementById("inputNombre").value;
     let inputApellido= document.getElementById("inputApellido").value;
