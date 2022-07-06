a08a85b06414c8968123c9f0abc4a9331fb2458c
Brisasanchez16 <brisncz@gmail.com>
2022-07-06 15:55:55 -0300

    Operadores-av

--- js/form.js
+++ js/form.js
@@ -2,11 +2,13 @@ function input(){
     let persona = {
         nombre:document.getElementById("inputNombre").value,
         direccion: document.getElementById("inputDireccion").value,
-        edad: document.getElementById("inputEdad").value
+        edad : document.getElementById("inputEdad").value
     }
     localStorage.setItem("Usuario", JSON.stringify(persona))
-    
     JSON.parse(localStorage.getItem(persona))
     console.log(persona)
+
+    let {edad} = persona;
+    edad >= 18 ? console.log("El envio fue aceptado ") : console.log("el envio fue rechazado")
 }
 
